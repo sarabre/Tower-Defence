b9ed4d8fb5fc0f64b958c31ed9f8883494393764
SaraBre <Sarabre26@gmail.com>
2024-02-28 05:04:44 -0800

    Better implementation of tile for better extension

--- Assets/Scripts/Ground/Tile/Tile.cs
+++ Assets/Scripts/Ground/Tile/Tile.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="Tile",menuName = "Ground/Tile")]
-
 public abstract class Tile : ScriptableObject
 {
     public Color32 BaseColor, OffsetColor;
---
+++ Assets/Scripts/Ground/Tile/TowerTile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TowerTile", menuName = "Ground/Tile")]
+
+public class TowerTile : Tile
+{
+   
+}
